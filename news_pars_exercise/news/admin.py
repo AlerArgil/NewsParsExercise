0adc65de43adf5bf6079f2b6c46df7eb41c7302c
Vlad <3second5mail4@gmail.com>
2022-09-02 21:09:41 +0300

    fix(add admin)

--- news_pars_exercise/news/admin.py
+++ news_pars_exercise/news/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from news_pars_exercise.news.models import Tag, New
+
+
+class NewAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(New, NewAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Tag, TagAdmin)
